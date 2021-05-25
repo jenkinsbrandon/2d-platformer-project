2783744dccd0a3315c5bee0490e0d593a97e9ff5
Brandon Jenkins <jenkinsb@sonoma.edu>
2021-05-25 14:15:20 -0700

    Simple Int Grid Implemented
    Created a simple grid with variable size, width, and height. Stores int that can be read by right clicking and increased by left clicking.

--- 2D Platformer Project/Assets/Scripts/TestingMap.cs
+++ 2D Platformer Project/Assets/Scripts/TestingMap.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class TestingMap : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Start is called before the first frame 
+    public int row = 20;
+    public int col = 10;
+    public float size = 10f;
+
+    private MapGrid grid;
     private void Start()
     {
-        MapGrid grid = new MapGrid(4,2,10f);
+
+       grid = new MapGrid(row,col,size, new Vector3(-20,-20));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if( Input.GetMouseButton(0))
+        {
+            grid.SetValue(Camera.main.ScreenToWorldPoint(Input.mousePosition), grid.GetValue(Camera.main.ScreenToWorldPoint(Input.mousePosition)) + 1);
+        }
+
+        if( Input.GetMouseButton(1))
+        {
+            Debug.Log(grid.GetValue(Camera.main.ScreenToWorldPoint(Input.mousePosition)));
+        }
     }
 }
