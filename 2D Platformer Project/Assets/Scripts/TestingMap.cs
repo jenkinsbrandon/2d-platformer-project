f547abc773038f6ff0d37d3a39c8caa26201c3b4
Brandon Jenkins <jenkinsb@sonoma.edu>
2021-05-25 20:33:32 -0700

    Grid now starts at bottom left camera position

--- 2D Platformer Project/Assets/Scripts/TestingMap.cs
+++ 2D Platformer Project/Assets/Scripts/TestingMap.cs
@@ -5,26 +5,27 @@ using UnityEngine;
 public class TestingMap : MonoBehaviour
 {
     // Start is called before the first frame 
-    public int row = 20;
-    public int col = 10;
-    public float size = 10f;
+    public int row = 50;
+    public int col = 25;
+    public float size = 3f;
+    Vector3 cameraDimensions;
 
     private MapGrid grid;
     private void Start()
     {
-
-       grid = new MapGrid(row,col,size, new Vector3(-20,-20));
+        cameraDimensions = Camera.main.ScreenToWorldPoint(new Vector3(0,0,10));
+        grid = new MapGrid(row,col,size, cameraDimensions);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if( Input.GetMouseButton(0))
+        if( Input.GetMouseButtonDown(0))
         {
             grid.SetValue(Camera.main.ScreenToWorldPoint(Input.mousePosition), grid.GetValue(Camera.main.ScreenToWorldPoint(Input.mousePosition)) + 1);
         }
 
-        if( Input.GetMouseButton(1))
+        if( Input.GetMouseButtonDown(1))
         {
             Debug.Log(grid.GetValue(Camera.main.ScreenToWorldPoint(Input.mousePosition)));
         }
