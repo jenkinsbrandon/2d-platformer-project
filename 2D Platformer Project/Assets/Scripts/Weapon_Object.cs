542eb27719b81cb199ef01b4838716c432f35952
Garrett Stahl <garrett_stahl@hotmail.com>
2021-05-31 21:32:31 -0700

    Some changes (???)

---
+++ 2D Platformer Project/Assets/Scripts/Weapon_Object.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace OurVideoGame
+{
+    using System;
+    using UnityEngine;
+    class Weapon_Object : MonoBehaviour
+    {
+        public Transform firePoint;
+        public float weaponDamage;
+
+        private void Update()
+        {
+            if(Input.GetButtonDown("space"))
+            {
+
+            }
+        }
+
+        private void ShootWeapon()
+        {
+
+        }
+    }
+}
