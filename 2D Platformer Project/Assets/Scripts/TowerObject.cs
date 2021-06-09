7b84ac1173dce38321ac53085873869de482a051
Brandon Jenkins <jenkinsb@sonoma.edu>
2021-06-08 17:46:38 -0700

    Towers now a unit object
    Got towers working with unit object. Added tower object. Removed map scripts and functionality.
    Still needs to be cleaned up

---
+++ 2D Platformer Project/Assets/Scripts/TowerObject.cs
@@ -0,0 +1,51 @@
+/// <summary>
+/// The superclass object that units (such as the player, enemies, and towers) will inherit.
+/// </summary>
+
+namespace OurVideoGame
+{
+	using System;
+	using UnityEngine;
+
+	public class TowerObject : Unit_Object
+    {
+        double movespeedModifier;
+        private Vector3 towerProjectileStart;
+        public ProjectileFireTest testPFT;
+        private double minimumTowerProximity = 10;
+        public Vector3 spawnPosition;
+
+        public GameObject towerPrefab;
+
+        public TowerObject(double newAttackDamage, double newMaxHealth, double pminimumTowerProximity, GameObject ptowerPrefab) : base(newAttackDamage, newMaxHealth)
+        {
+            this.movespeedModifier = 0;
+            this.minimumTowerProximity = pminimumTowerProximity;
+            this.towerPrefab = ptowerPrefab;
+        }
+
+        public double MinimumTowerProximity
+		{
+			get => this.minimumTowerProximity;
+			set => this.minimumTowerProximity = value;
+		}
+
+        public bool SpawnTower( Vector3 pspawnPosition )
+        {
+
+            Instantiate(towerPrefab, pspawnPosition, Quaternion.identity);
+            spawnPosition = pspawnPosition;
+
+            return false;
+        }
+
+        public override void Unit_Attack()
+        {
+
+        }
+
+    }
+
+    
+
+}
---
+++ 2D Platformer Project/Assets/Scripts/Unit_Object.cs
@@ -0,0 +1,43 @@
+/// <summary>
+/// The superclass object that units (such as the player, enemies, and towers) will inherit.
+/// </summary>
+
+namespace OurVideoGame
+{
+	using System;
+	using UnityEngine;
+	abstract public class Unit_Object : MonoBehaviour
+	{
+		private double unitAttackDamage;
+		private double unitCurrentHealth;
+		private double unitMaxHealth;
+
+		public Unit_Object(double newAttackDamage, double newMaxHealth)
+		{
+			this.unitAttackDamage = newAttackDamage;
+			this.unitMaxHealth = newMaxHealth;
+			this.unitCurrentHealth = newMaxHealth;
+		}
+
+		public double AttackDamage
+		{
+			get => this.unitAttackDamage;
+			set => this.unitAttackDamage = value;
+		}
+
+		public double CurrentHealth
+		{
+			get => this.unitCurrentHealth;
+			set => this.unitCurrentHealth = value;
+		}
+
+		public double MaxHealth
+		{
+			get => this.unitMaxHealth;
+			set => this.unitMaxHealth = value;
+		}
+
+		// Create an attack_object and throw it into the world.	
+		abstract public void Unit_Attack();
+	}
+}
